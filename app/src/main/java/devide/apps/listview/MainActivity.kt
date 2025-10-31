1b530b6c16c3716d27a606bf795e26590fab2c8e
D3VIDE <errandojose03@gmail.com>
2025-10-31 21:01:48 +0700

    Add: Update Dialog

--- app/src/main/java/devide/apps/listview/MainActivity.kt
+++ app/src/main/java/devide/apps/listview/MainActivity.kt
@@ -1,11 +1,18 @@
 package devide.apps.listview
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -50,5 +57,106 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+        val gestureDetector = GestureDetector(
+            this,
+            object : GestureDetector.SimpleOnGestureListener(){
+                override fun onDoubleTap(e: MotionEvent): Boolean {
+                    val position = _lvl1.pointToPosition(e.x.toInt(),e.y.toInt())
+                    if(position != ListView.INVALID_POSITION){
+                        val selectedItem = data[position]
+                        showActionDialog(position,selectedItem,data,lvadapter)
+                    }
+                    return true
+                }
+
+
+            }
+        )
+    }
+
+    private fun showActionDialog(
+        position: Int,
+        selectedItem: String,
+        data: MutableList<String>,
+        lvadapter: ArrayAdapter<String>
+    ) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ITEM $selectedItem")
+        builder.setMessage("Pilih tindakan:")
+
+        builder.setPositiveButton("Update") { _, _ ->
+            showUpdateDialog(position, selectedItem,data,adapter)
+        }
+
+        builder.setNegativeButton("Hapus"){_,_ ->
+            data.removeAt(position)
+            adapter.notifyDataSetChanged()
+            Toast.makeText(
+                this,
+                "Hapus Item $selectedItem",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.setNeutralButton("Batal"){dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
+    }
+
+    private fun showUpdateDialog(
+        position: Int,
+        oldValue: String,
+        data: MutableList<String>,
+        adapter: ArrayAdapter<String>
+    ){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Update Data")
+
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
+        layout.setPadding(50,40,50,10)
+
+        val tvOld = TextView(this)
+        tvOld.text = "Data lama: $oldValue"
+        tvOld.textSize = 16f
+
+        val etNew = EditText(this)
+        etNew.hint = "Masukan data baru"
+        etNew.setText(oldValue)
+
+        layout.addView(tvOld)
+        layout.addView(etNew)
+
+        builder.setView(layout)
+        builder.setPositiveButton("Simpan") { dialog, _ ->
+            val newValue = etNew.text.toString().trim()
+            if (newValue.isNotEmpty()) {
+                data[position] = newValue
+                adapter.notifyDataSetChanged()
+                Toast.makeText(
+                    this,
+                    "Data diupdate jadi: $newValue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Data baru tidak boleh kosong!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("Batal") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
+
     }
 }
+
