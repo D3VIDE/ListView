9dbc0301de3471abf99592dba6de01417ee0e383
D3VIDE <errandojose03@gmail.com>
2025-10-31 16:25:43 +0700

    Add: ListView and Display

--- app/src/main/java/devide/apps/listview/MainActivity.kt
+++ app/src/main/java/devide/apps/listview/MainActivity.kt
@@ -1,12 +1,17 @@
 package devide.apps.listview
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var data = mutableListOf<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        data.addAll(listOf("1", "2", "3", "4", "5"))
+
+        val lvadapter = ArrayAdapter(
+        this,
+        android.R.layout.simple_list_item_1,
+        data)
+
+        val _lvl1 = findViewById<ListView>(R.id.lv1)
+        _lvl1.adapter = lvadapter
     }
 }
